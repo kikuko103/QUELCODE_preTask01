53b5971f10d582c2f38b89ad6a9914d77a4b1baa
kikuko103 <kiku345602@icloud.com>
2020-03-06 16:14:37 +0900

    アコーディオン部分修正

--- main.js
+++ main.js
@@ -11,20 +11,19 @@ const qaToggle = (str) => {
 // SP版のヘッダーアニメーション(ハンバーガー　＋　アコーディオン)
 const humberger = document.getElementById('menuTrigger');
 const accordion = document.getElementById('headerNav');
+const list = document.getElementsByClassName('list_item');
+const anchor = document.getElementsByClassName('anchor');
 
 humberger.onclick = () => {
     humberger.classList.toggle('menuTrigger_opened');
     accordion.classList.toggle('header_nav_opened');
-};
-
-//
-//
-//
-
-
-
-
+    for (let i = 0; i < list.length; i++) {
+        list[i].classList.toggle('list_item_opened');
+    }
+    for (let i = 0; i < list.length; i++) {
+        anchor[i].classList.toggle('anchor_opened');
+    }
+}
 
 // 回答がでてくるときのフェードイン・アウトもやりましょうか！
 // 余力あればPC版のページ切替時のフェードインも！
-
