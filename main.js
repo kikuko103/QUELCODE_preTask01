d5404a82bfceb38401530b15ed7e815e2479285c
kikuko103 <kiku345602@icloud.com>
2020-03-09 12:03:47 +0900

    for文の重なりを直しました

--- main.js
+++ main.js
@@ -1,38 +1,35 @@
-'use strict';
+"use strict";
 // QAページ回転アニメーション
-const qaToggle = (str) => {
-    const arrow = document.getElementById('qa_icon' + str);
-    const qa = document.getElementById('qaBox_closed' + str);
+const qaToggle = str => {
+  const arrow = document.getElementById("qa_icon" + str);
+  const qa = document.getElementById("qaBox_closed" + str);
 
-    arrow.classList.toggle('qa_rotatte');
-    qa.classList.toggle('qaBox_opened');
-}
+  arrow.classList.toggle("qa_rotatte");
+  qa.classList.toggle("qaBox_opened");
+};
 
 // SP版のヘッダーアニメーション(ハンバーガー　＋　アコーディオン)
-const humberger = document.getElementById('menuTrigger');
-const accordion = document.getElementById('headerNav');
-const list = document.getElementsByClassName('list_item');
-const anchor = document.getElementsByClassName('anchor');
+const humberger = document.getElementById("menuTrigger");
+const accordion = document.getElementById("headerNav");
+const list = document.getElementsByClassName("list_item");
+const anchor = document.getElementsByClassName("anchor");
 
 humberger.onclick = () => {
-    humberger.classList.toggle('menuTrigger_opened');
-    accordion.classList.toggle('header_nav_opened');
-    for (let i = 0; i < list.length; i++) {
-        list[i].classList.toggle('list_item_opened');
-    }
-    for (let i = 0; i < list.length; i++) {
-        anchor[i].classList.toggle('anchor_opened');
-    }
-}
+  humberger.classList.toggle("menuTrigger_opened");
+  accordion.classList.toggle("header_nav_opened");
+  for (let i = 0; i < list.length; i++) {
+    list[i].classList.toggle("list_item_opened");
+    anchor[i].classList.toggle("anchor_opened");
+  }
+};
 
-// 余力あればPC版のページ切替時のフェードインも！
-const a = document.querySelectorAll('a.anchor');
+//ヘッダーナビのアニメーション
+const a = document.querySelectorAll("a.anchor");
 const pageUrl = location.href;
 for (let i = 0; i < a.length; i++) {
-    const href = a[i].getAttribute('href');
-    if (pageUrl.includes(href)) {
-        console.log('match');
-        a[i].classList.toggle('anchor_position');
-    }
+  const href = a[i].getAttribute("href");
+  if (pageUrl.includes(href)) {
+    console.log("match");
+    a[i].classList.toggle("anchor_position");
+  }
 }
-
